fdf191c1ea534edb03d9e0a0a18d26e71628fd25
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-06-03 23:34:12 +0300

    #3. Добавление класса StudentContext.
    Добавления класса StudentContext для взаимодействия с сервером БД. В нем содержится класс Student.

---
+++ Git/Models/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Git.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public StudentContext(DbContextOptions<StudentContext> options)
+           : base(options)
+        {
+            if (Database.EnsureCreated())
+            {
+                Students.Add(new Student { Name = "Иван", Surname = "Иванов", Age = 20, GPA = 10.5f });
+                Students.Add(new Student { Name = "Сергей", Surname = "Сергеев", Age = 23, GPA = 11.5f });
+                Students.Add(new Student { Name = "Петр", Surname = "Петров", Age = 25, GPA = 12f });
+                SaveChanges();
+            }
+        }
+    }
+}
