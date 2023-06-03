6e2005efcf01f9b9e31d9c0009b0bae24427d7fc
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-06-03 22:59:46 +0300

    #1 Добавил класc Students
    Класс предназначен для описания сущности студента. На основе класса будет создана таблица Students

---
+++ Git/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Git.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        // Имя студента
+        public string Name { get; set; }
+        // Фамилия студента
+        public string Surname { get; set; }
+        // Возраст студента
+        public int Age { get; set; }
+        // Средний балл
+        public float GPA { get; set; }
+    }
+}
