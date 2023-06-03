0b373daa0b529aa3d31c205ccdb5fde7229adf3c
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-06-03 23:44:24 +0300

    #4 Добавление класса StudentsController
    Добавление файла StudentsController cs с классом
    StudentsController
    Класс поедназначен для обработки запросов клиентов.
    В классе создан метод Index, который возвращает
    стартовию страницу приложения.

---
+++ Git/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Git.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Git.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private readonly StudentContext _context;
+
+        public StudentController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Students != null ?
+                        View(await _context.Students.ToListAsync()) :
+                        Problem("Entity set 'StudentContext.Students'  is null.");
+        }
+
+    }
+}
