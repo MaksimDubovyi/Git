b6f139c4cc9858d86b373e7505d1d03fdec58e8f
MaksimDubovyi <maksim.dubovyi.ua@gmail.com>
2023-06-04 00:58:36 +0300

    #8  Добавление действия Edit
    Добавление в класс контроддера двух методов Edit
    Один из методов позволяет на отдельной странице изменить данные о студенте
    Второй метод синхронивиоует изменения с базой

--- Git/Controllers/StudentController.cs
+++ Git/Controllers/StudentController.cs
@@ -58,5 +58,57 @@ namespace Git.Controllers
             }
             return View(student);
         }
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,Age,GPA")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+        private bool StudentExists(int id)
+        {
+            return (_context.Students?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
